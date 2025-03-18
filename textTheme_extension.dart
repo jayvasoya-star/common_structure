86f3e6813e9ce9091b2e5734d0f92fa4d79882ab
jayvasoya-star <156075552+jayvasoya-star@users.noreply.github.com>
2025-03-18 22:47:12 +0530

    Create textTheme_extension.dart

---
+++ textTheme_extension.dart
@@ -0,0 +1,316 @@
+extension TextThemeExtension on TextTheme {
+
+  TextStyle get heading1Bold => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 60.sp,
+      );
+  TextStyle get heading1Semi => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 60.sp,
+      );
+  TextStyle get heading1Medium => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 60.sp,
+      );
+  TextStyle get heading1Regular => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 60.sp,
+      );
+
+  TextStyle get heading2Bold => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 48.sp,
+      );
+  TextStyle get heading2Semi => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 48.sp,
+      );
+  TextStyle get heading2Medium => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 48.sp,
+      );
+  TextStyle get heading2Regular => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 48.sp,
+      );
+
+  TextStyle get heading3Bold => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 36.sp,
+      );
+  TextStyle get heading3Semi => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 36.sp,
+      );
+  TextStyle get heading3Medium => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 36.sp,
+      );
+  TextStyle get heading3Regular => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 36.sp,
+      );
+
+  TextStyle get heading4Bold => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 24.sp,
+      );
+  TextStyle get heading4Semi => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 24.sp,
+      );
+  TextStyle get heading4Medium => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 24.sp,
+      );
+  TextStyle get heading4Regular => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 24.sp,
+      );
+
+  TextStyle get heading5Bold => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 20.sp,
+      );
+  TextStyle get heading5Semi => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 20.sp,
+      );
+  TextStyle get heading5Medium => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 20.sp,
+      );
+  TextStyle get heading5Regular => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 20.sp,
+      );
+
+  TextStyle get heading6Bold => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 16.sp,
+      );
+  TextStyle get heading6Semi => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 16.sp,
+      );
+  TextStyle get heading6Medium => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 16.sp,
+      );
+  TextStyle get heading6Regular => displayMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 16.sp,
+      );
+
+  TextStyle get subTitle1Bold => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 14.sp,
+      );
+  TextStyle get subTitle1Semi => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 14.sp,
+      );
+  TextStyle get subTitle1Medium => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 14.sp,
+      );
+  TextStyle get subTitle1Regular => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 14.sp,
+      );
+
+  TextStyle get subTitle2Bold => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 12.sp,
+      );
+  TextStyle get subTitle2Semi => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 12.sp,
+      );
+  TextStyle get subTitle2Medium => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 12.sp,
+      );
+  TextStyle get subTitle2Regular => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 12.sp,
+      );
+
+  TextStyle get subTitle3Bold => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 10.sp,
+      );
+  TextStyle get subTitle3Semi => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 10.sp,
+      );
+  TextStyle get subTitle3Medium => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 10.sp,
+      );
+  TextStyle get subTitle3Regular => titleMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 10.sp,
+      );
+
+  TextStyle get body1Bold => bodyLarge!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 14.sp,
+      );
+  TextStyle get body1Semi => bodyLarge!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 14.sp,
+      );
+  TextStyle get body1Medium => bodyLarge!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 14.sp,
+      );
+  TextStyle get body1Regular => bodyLarge!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 14.sp,
+      );
+
+  TextStyle get body2Bold => bodyMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 12.sp,
+      );
+  TextStyle get body2Semi => bodyMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 12.sp,
+      );
+  TextStyle get body2Medium => bodyMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 12.sp,
+      );
+  TextStyle get body2Regular => bodyMedium!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 12.sp,
+      );
+  TextStyle get caption1Bold => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 10.sp,
+      );
+  TextStyle get caption1Semi => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 10.sp,
+      );
+  TextStyle get caption1Medium => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 10.sp,
+      );
+  TextStyle get caption1Regular => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 10.sp,
+      );
+
+  TextStyle get caption2Bold => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 8.sp,
+      );
+  TextStyle get caption2Semi => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 8.sp,
+      );
+  TextStyle get caption2Medium => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 8.sp,
+      );
+  TextStyle get caption2Regular => bodySmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 8.sp,
+      );
+
+  TextStyle get overline1Bold => labelSmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 10.sp,
+      );
+  TextStyle get overline1Semi => labelSmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 10.sp,
+      );
+  TextStyle get overline1Medium => labelSmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 10.sp,
+      );
+  TextStyle get overline1Regular => bodyLarge!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 10.sp,
+      );
+
+  TextStyle get overline2Bold => labelSmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w700,
+        fontSize: 8.sp,
+      );
+  TextStyle get overline2Semi => labelSmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w600,
+        fontSize: 8.sp,
+      );
+  TextStyle get overline2Medium => labelSmall!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w500,
+        fontSize: 8.sp,
+      );
+  TextStyle get overline2Regular => bodyLarge!.copyWith(
+        color: appColors.neutral1Color,
+        fontWeight: FontWeight.w400,
+        fontSize: 8.sp,
+      );
+}
