d51ffcfd31d543562e164b415ca18b4a0756c385
jaydeep vasoya <156075552+jayvasoya-star@users.noreply.github.com>
2025-03-19 20:44:00 +0530

    Update sizedbox_extension.dart

--- sizedbox_extension.dart
+++ sizedbox_extension.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 extension SizedBoxExtension on num {
   SizedBox get heightBox => SizedBox(height: h);
   SizedBox get widthBox => SizedBox(width: w);
